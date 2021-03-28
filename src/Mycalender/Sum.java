1a26816049b33e8af5072cd1f91a31192d0415e7
twozeropang <pma5758@naver.com>
2021-03-28 19:33:06 +0900

    Sum

---
+++ src/Mycalender/Sum.java
@@ -0,0 +1,25 @@
+package Mycalender;
+
+import java.util.Scanner;
+
+public class Sum {
+	public static void main(String[] args) {
+		//입력: 키보드로 두 수의 입력을 받는다.
+		//출력: 와면에 두 수의 합을 출력한다.
+		int a,b;
+		Scanner sc = new Scanner(System.in);
+		String s1, s2;
+		System.out.println("두 수를 입력하세요~!");
+		s1 = sc.next();
+		s2 = sc.next();
+		System.out.println(s1 + ", " +s2);
+		a = Integer.parseInt(s1);
+		b = Integer.parseInt(s2);
+		//System.out.println(a + ", " + b);
+		
+		//int c = a+b;
+//		System.out.println("두 수의 합은" + (a + b) + "입니다.");
+		System.out.printf("%d와 %d의 합은 %d입니다.", a, b, a+b);
+		sc.close();
+	}
+}
