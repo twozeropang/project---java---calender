17ce314a9abbe9bff54e31c47d84d1055bffedb1
twozeropang <pma5758@naver.com>
2021-03-28 20:05:54 +0900

    Implement find days of month

--- src/Mycalender/Calender.java
+++ src/Mycalender/Calender.java
@@ -1,5 +1,7 @@
 package Mycalender;
 
+import java.util.Scanner;
+
 public class Calender {
 	public static void main(String[] args) {
 		System.out.println("일 월 화 수 목 금 토");
@@ -8,5 +10,23 @@ public class Calender {
 		System.out.print("8  9  10 11 12 13 14\n");
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
+		
+		//숫자를 입력받아 해당하는 달의 최대 일수를 출겨하는 프로그램
+		System.out.println("---------------------------\n");
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("알고 싶은 월을 입력하세요");
+		int month = sc.nextInt();
+		
+		int[] maxDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+		
+		System.out.printf("%d월은 %d일까지 있습니다.\n", month, maxDays[month-1]);
+		sc.close();
+//		if(num%2==0) {
+//			System.out.printf("%d월의 최대 일수는 %d일입니다.", num, 30 );
+//		}else {
+//			System.out.printf("%d월의 최대 일수는 %d입니다.", num, 28);
+//		}
 	}
 }
+
