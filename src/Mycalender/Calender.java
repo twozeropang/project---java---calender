8761538de774b979883a3831f8f775f8a84eac28
twozeropang <pma5758@naver.com>
2021-03-28 18:40:20 +0900

    Update Calender main()

--- src/Mycalender/Calender.java
+++ src/Mycalender/Calender.java
@@ -2,6 +2,11 @@ package Mycalender;
 
 public class Calender {
 	public static void main(String[] args) {
-		System.out.println("Hello Calender!");
+		System.out.println("일 월 화 수 목 금 토");
+		System.out.println("-------------------");
+		System.out.print("1  2  3  4  5  6  7\n");
+		System.out.print("8  9  10 11 12 13 14\n");
+		System.out.println("15 16 17 18 19 20 21");
+		System.out.println("22 23 24 25 26 27 28");
 	}
 }
