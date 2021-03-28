c3c3f3a7b24db8f51efe7b1227537181cacb4d54
twozeropang <pma5758@naver.com>
2021-03-28 20:25:05 +0900

    Calendarswitch

--- src/Mycalender/Calender.java
+++ src/Mycalender/Calender.java
@@ -4,10 +4,21 @@ import java.util.Scanner;
 
 public class Calender {
 	// 함수로 보기 좋게 빼놓은 형식(메소드 형식)
-	private static final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-	public int GetMaxDaysOfMonth(int month) { 
-		return MAX_DAYS[month -1];
+	private static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+	public int GetMaxDaysOfMonth(int month) {
+
+		switch (month) {
+		case 2:
+			return 28;
+		case 4:
+			return 30;
+		default:
+			return 31;
+		}
+//		return MAX_DAYS[month -1];
 	}
+
 	public void printSampleCalendar() {
 		System.out.println("일 월 화 수 목 금 토");
 		System.out.println("-------------------");
@@ -15,23 +26,24 @@ public class Calender {
 		System.out.print("8  9  10 11 12 13 14\n");
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
-		
+
 	}
+
 	public static void main(String[] args) {
-	
-		//숫자를 입력받아 해당하는 달의 최대 일수를 출겨하는 프로그램
+
+		// 숫자를 입력받아 해당하는 달의 최대 일수를 출겨하는 프로그램
 		System.out.println("---------------------------\n");
-		
+
 		Scanner sc = new Scanner(System.in);
 		Calender cal = new Calender();
 		System.out.println("알고 싶은 월을 입력하세요");
 		int month = sc.nextInt();
-		
+
 //		int[] maxDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-		
+
 //		System.out.printf("%d월은 %d일까지 있습니다.\n", month, maxDays[month-1]);
 		System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.GetMaxDaysOfMonth(month));
-		
+
 		cal.printSampleCalendar();
 		sc.close();
 //		if(num%2==0) {
@@ -41,4 +53,3 @@ public class Calender {
 //		}
 	}
 }
-
